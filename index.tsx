f30b358b19ccd8d58c06cb2f70afaf53e129ce2c
Joshua Harms <joshua@nozzlegear.com>
2025-01-28 16:19:44 -0600

    refactor: Modernize component using functional syntax and hooks
    Converted class-based component to functional component using React.useMemo for id generation.
    
    Simplified rendering logic and improved TypeScript typing by using utility functions.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
-export interface IProps {
+export type Props = {
     color?: string;
     size?: number | string;
     margin?: string;
@@ -10,126 +9,104 @@ export interface IProps {
     ref?: React.Ref<any>;
 }
 
-export class Dialog extends React.Component<IProps, any>
-{
-    constructor(props: IProps, context) {
-        super(props, context);
-    }
+export const SVG_STYLE: React.CSSProperties = {
+    animation: 'rotate 2s linear infinite',
+    height: '100%',
+    transformOrigin: 'center center',
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto'
+};
 
-    static propTypes: Record<keyof IProps, React.Requireable<any>> = {
-        key: React.PropTypes.any,
-        ref: React.PropTypes.any,
-        color: React.PropTypes.string,
-        size: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-        margin: React.PropTypes.string,
-        strokeWidth: React.PropTypes.number,
+/**
+ * Creates a unique uuid. Based off of https://stackoverflow.com/a/2117523.
+ */
+function createUuid(): string {
+    const input = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+    return input.replace(/[xy]/g, c => {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+
+        return v.toString(16);
+    });
+}
+
+function getCircleStyle(id: string): Record<string, string> {
+    return {
+        strokeDasharray: '1,200',
+        strokeDashoffset: '0',
+        animation: `dash 1.5s ease-in-out infinite, color-${id} 6s ease-in-out infinite`,
+        strokeLinecap: 'round'
     };
+}
 
-    private svg_style = {
-        animation: 'rotate 2s linear infinite',
-        height: '100%',
-        transformOrigin: 'center center',
-        width: '100%',
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        margin: 'auto'
+function getContainerStyle(size: number | string = 16): Record<string, string | number> {
+    const sizeValue = typeof (size) === "number" ? `${size}px` : size;
+    return {
+        position: 'relative',
+        width: sizeValue,
+        height: sizeValue,
     };
+}
 
-    /**
-     * Creates a unique guid. Based off of https://stackoverflow.com/a/2117523.
-     */
-    private createGuid(): string {
-        const input = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
-
-        return input.replace(/[xy]/g, c => {
-            const r = Math.random() * 16 | 0;
-            const v = c === 'x' ? r : (r & 0x3 | 0x8);
-
-            return v.toString(16);
-        });
-    }
-
-    private getCircleStyle(id: string) {
-        const style = {
-            strokeDasharray: '1,200',
-            strokeDashoffset: '0',
-            animation: `dash 1.5s ease-in-out infinite, color-${id} 6s ease-in-out infinite`,
-            strokeLinecap: 'round'
-        };
-
-        return style;
-    };
-
-    private getStyle(size: number | string = 16) {
-        const sizeValue = typeof (size) === "number" ? `${size}px` : size;
-        const loading_style = {
-            position: 'relative',
-            width: size,
-            height: size,
-        };
-
-        return loading_style;
-    }
-
-    private getAnimation(id: string, color: string = `#00bcd4`) {
-        const animation = `
-            @keyframes rotate {
-                100% {
-                    transform: rotate(360deg);
-                }
+function getAnimationKeyframes(id: string, color: string = `#00bcd4`): string {
+    return `
+        @keyframes rotate {
+            100% {
+                transform: rotate(360deg);
             }
-            @keyframes dash {
-                0% {
-                    stroke-dasharray: 1,200;
-                    stroke-dashoffset: 0;
-                }
-                50% {
-                    stroke-dasharray: 89,200;
-                    stroke-dashoffset: -35px;
-                }
-                100% {
-                    stroke-dasharray: 89,200;
-                    stroke-dashoffset: -124px;
-                }
+        }
+        @keyframes dash {
+            0% {
+                stroke-dasharray: 1,200;
+                stroke-dashoffset: 0;
             }
-            @keyframes color-${id} {
-                100%, 0% {
-                    stroke: ${color};
-                }
-                40% {
-                    stroke: ${color};
-                }
-                66% {
-                    stroke: ${color};
-                }
-                80%, 90% {
-                    stroke: ${color};
-                }
-            }`;
+            50% {
+                stroke-dasharray: 89,200;
+                stroke-dashoffset: -35px;
+            }
+            100% {
+                stroke-dasharray: 89,200;
+                stroke-dashoffset: -124px;
+            }
+        }
+        @keyframes color-${id} {
+            100%, 0% {
+                stroke: ${color};
+            }
+            40% {
+                stroke: ${color};
+            }
+            66% {
+                stroke: ${color};
+            }
+            80%, 90% {
+                stroke: ${color};
+            }
+        }`;
+}
 
-        return animation;
-    }
 
+export function Dialog(this: unknown, props: Props): React.ReactElement {
+    const { size, color, margin, strokeWidth } = props;
+    const id = React.useMemo(() => createUuid(), []);
+    const divStyle = getContainerStyle(size);
+    const circleStyle = getCircleStyle(id);
+    const animationKeyframes = getAnimationKeyframes(id, color);
 
-    render() {
-        const { size, color, margin, strokeWidth } = this.props;
-        const id = this.createGuid();
-        const style = this.getStyle(size);
-        const circleStyle = this.getCircleStyle(id);
-        const animation = this.getAnimation(id, color);
-
-        return (
-            <div className={`svg react-svg-progress`} style={{ ...style, display: "inline-block", margin }}>
-                <style>{animation}</style>
-                <svg style={this.svg_style} viewBox="25 25 50 50">
-                    <circle style={circleStyle} cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth || 5} strokeMiterlimit="10" />
-                </svg>
-            </div>
-        );
-    }
+    return (
+        <div className={"svg react-svg-progress"} style={{ ...divStyle, display: "inline-block", margin }}>
+            <style>{animationKeyframes}</style>
+            <svg style={SVG_STYLE} viewBox="25 25 50 50">
+                <circle style={circleStyle} cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth || 5} strokeMiterlimit="10" />
+            </svg>
+        </div>
+    );
 }
 
 export default Dialog;
